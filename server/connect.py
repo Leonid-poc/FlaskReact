13291074ba05ec4f061e3028b76aa84ef2403785
Leonid-poc <legeorg2004@gmail.com>
2023-11-27 10:13:29 +0500

    connected Data Base and adding signup

--- server/connect.py
+++ server/connect.py
@@ -1,7 +1,11 @@
 import psycopg2 as psg
 
+
 try:
-    psg.connect(database="delivery_base", user='postgres', password='1234')
-    print('Connected!')
+    # /var/lib/pgsql/data/pg_hba.conf --> (peer -> md5)
+    conn = psg.connect(database="delivery_base", user='postgres', password='1234')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM users')
+    print(cursor.fetchone())
 except Exception as e:
     print(e)
